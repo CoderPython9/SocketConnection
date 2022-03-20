82626933deae06ef4e63c0d62519e0e00b5fe71a
CoderPython9 <101985760+CoderPython9@users.noreply.github.com>
2022-03-20 19:06:39 +0000

    Client File

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+class Sock(object):
+	def __init__(self, ip, port=12345):
+		self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self._client.connect((ip, port))
+		
+	def send(self, message):
+		self._client.sendall(message)
