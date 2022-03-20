466adf1bc65dcbb5a965b5da15c18e854b7de3f1
CoderPython9 <101985760+CoderPython9@users.noreply.github.com>
2022-03-20 18:54:21 +0000

    Server File

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+class Sock(object):
+	def __init__(self, hostname="", port=12345):
+		self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self._server.bind((hostname, port))
+		conn, addr = self._server.listen(5)
+		self._conn = conn
+		self.addr = addr
+	def send(self, message):
+		self._conn.send(message)
+	def receive(self):
+		return str(self._conn.recv(1024), 'utf8')
+	def disconnect(self):
+		self._conn.close()
